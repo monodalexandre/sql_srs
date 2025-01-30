97a2bd70a57514d71e66531e77ea7e36d8b4d60d
monodalexandre <monodalexandre@yahoo.fr>
2025-01-30 21:31:23 +0800

    karcher

--- app.py
+++ app.py
@@ -26,37 +26,3 @@ CROSS JOIN food_items
 """
 
 
-with st.sidebar:
-    option = st.selectbox(
-        "What would you like to review ?"
-        , ("Joins", "GroupBy", "Window functions")
-        , index=None
-        , placeholder="Select a theme"
-    )
-
-    st.write("You selected", option)
-
-
-
-print("Je printe ça là")
-
-st.header("enter your code")
-query = st.text_area(label="votre code SQL ici", key="user_input")
-if query:
-    result = duckdb.sql(query).df()
-    st.dataframe(result)
-
-tab2, tab3  = st.tabs(["Tables", "Solution"])
-
-with tab2:
-    st.write("table: beverages")
-    st.dataframe(beverages)
-    st.write("table: food_items")
-    st.dataframe(food_items)
-    st.write("expected:")
-    st.dataframe(solution)
-
-with tab3:
-    st.write(answer)
-
-
