5b466cccc8d8e3ea3224b9f3a0d5bc2b9a39f11d
monodalexandre <monodalexandre@yahoo.fr>
2025-01-30 17:28:43 +0800

    Add dataframes

--- app.py
+++ app.py
@@ -1,11 +1,51 @@
 import duckdb
 import pandas as pd
 import streamlit as st
+import io
 
-st.write("""
-# SQL SRS
-Spaced Repetition System SQL practice
-""")
+csv = '''
+beverage,price
+orange juice,2.5
+Expresso,2
+Tea,3
+'''
+
+beverages = pd.read_csv(io.StringIO(csv))
+
+csv2 = '''
+food_item,food_price
+cookie juice,2.5
+chocolatine,2
+muffin,3
+'''
+
+food_items = pd.read_csv(io.StringIO(csv2))
+
+answer = """
+SELECT * FROM beverages
+CROSS JOIN food_items
+"""
+
+solution = duckdb.sql(answer).df()
+
+st.header("enter your code")
+query = st.text_area(label="votre code SQL ici", key="user_input")
+if query:
+    result = duckdb.sql(query).df()
+    st.dataframe(result)
+
+tab2, tab3  = st.tabs(["Tables", "Solution"])
+
+with tab2:
+    st.write("table: beverages")
+    st.dataframe(beverages)
+    st.write("table: food_items")
+    st.dataframe(food_items)
+    st.write("expected:")
+    st.dataframe(solution)
+
+with tab3:
+    st.write(answer)
 
 with st.sidebar:
     option = st.selectbox(
@@ -16,10 +56,6 @@ with st.sidebar:
     )
 
     st.write("You selected", option)
-
-data = {"a": [1,2,3], "b":[4,5,6]}
-df = pd.DataFrame(data)
-
 
 sql_query = st.text_area(label="Requête SQL")
 result = duckdb.sql(sql_query)
